1227dab073b35ddb10ecc119b955293050525931
raghuworld1992 <raghuworld1992@gmail.com>
2022-11-12 09:12:29 +0530

    Adding launch videos .. In progress

--- src/app/features/launches/components/launch-detail/launch-detail.component.ts
+++ src/app/features/launches/components/launch-detail/launch-detail.component.ts
@@ -3,7 +3,13 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { LaunchService } from '@features/launches/launch.service';
 import { LaunchDetail } from '@models/launches/launchDetail.model';
 import { Subscription } from 'rxjs';
-import { faArrowLeft, faMapMarkerAlt, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faMapMarkerAlt,
+  faCalendarDay,
+  faVideo,
+  faVideoSlash,
+} from '@fortawesome/free-solid-svg-icons';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SimpleModalComponent } from '@shared/components/simple-modal/simple-modal.component';
 import { LaunchUtilService } from '@shared/services/launch-Util.service';
@@ -18,8 +24,8 @@ export class LaunchDetailComponent implements OnInit, OnDestroy {
     private _route: ActivatedRoute,
     private modalService: NgbModal,
     private _launchService: LaunchService,
-    private _launchutilService:LaunchUtilService
-  ) { }
+    private _launchutilService: LaunchUtilService
+  ) {}
 
   private slug: string | null;
   private _launchServiceSubs: Subscription;
@@ -28,29 +34,37 @@ export class LaunchDetailComponent implements OnInit, OnDestroy {
   faArrowLeft = faArrowLeft;
   faMapMarkerAlt = faMapMarkerAlt;
   faCalendarDay = faCalendarDay;
+  faVideo = faVideo;
+  faVideoSlash = faVideoSlash;
 
-  launchStatusColor:string;
+  launchStatusColor: string;
 
   ngOnInit(): void {
     this._route.paramMap.subscribe((data: ParamMap) => {
       this.slug = data.get('slug');
     });
 
-    this._launchServiceSubs = this._launchService.getLaunchInfo(this.slug!).subscribe({
-      next: (data) => {
-        this.launchInfo = data;
-        this.launchStatusColor =  this._launchutilService.getLaunchStatusColor(data.status.abbrev);        
-      }
-    });
+    this._launchServiceSubs = this._launchService
+      .getLaunchInfo(this.slug!)
+      .subscribe({
+        next: (data) => {
+          this.launchInfo = data;
+          this.launchStatusColor = this._launchutilService.getLaunchStatusColor(
+            data.status.abbrev
+          );
+        },
+      });
   }
 
   ngOnDestroy(): void {
     this._launchServiceSubs?.unsubscribe();
   }
 
-
   open(text: string | undefined) {
-    const modalRef = this.modalService.open(SimpleModalComponent, { scrollable: true, centered: true });
-    modalRef.componentInstance.text = text;    
+    const modalRef = this.modalService.open(SimpleModalComponent, {
+      scrollable: true,
+      centered: true,
+    });
+    modalRef.componentInstance.text = text;
   }
 }
