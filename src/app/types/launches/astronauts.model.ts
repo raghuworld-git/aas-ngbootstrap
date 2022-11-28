d669803f97cd793a56fe8cfd841d57da3a593329
raghuworld1992 <raghuworld1992@gmail.com>
2022-11-28 15:02:03 +0530

    Adding addtional properties to Astronauts model

--- src/app/types/launches/astronauts.model.ts
+++ src/app/types/launches/astronauts.model.ts
@@ -1,10 +1,24 @@
 export interface Astronaut {
-         role:{role:string,priority:number},
-         astronaut : {
-                id:number,
-                name:string,
-                profile_image:string | null,
-                launchStagesIncluded:string
-        },
-         crewGroup?:string[],
+  role: { role: string; priority: number };
+  astronaut: {
+    id: number;
+    name: string;
+    profile_image: string | null;
+    launchStagesIncluded: string;
+    status: config;
+    type:config,
+    date_of_birth:string,
+    date_of_death:string | null,
+    nationality:string,
+    twitter:string| null,
+    wiki:string | null,
+    instagram:string | null,
+    bio:string | null
+  };
+  crewGroup?: string[];
+}
+
+interface config {
+  id: number;
+  name: string;
 }
